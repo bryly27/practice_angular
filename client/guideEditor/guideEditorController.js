cb544de82429f8cae7ca6c28be194f07213d0d05
Bryant <bryly27@sbcglobal.net>
2015-12-04 22:35:06 -0800

    added sidebar code into guide editor controller

--- client/guideEditor/guideEditorController.js
+++ client/guideEditor/guideEditorController.js
@@ -4,28 +4,40 @@ cg.controller('guideEditorController', function($scope){
 
 
 	$scope.updateSidebar = function(guide){
-		console.log(guide);
+		// console.log(guide);
 	};
 
 
 
 
+    // Sticky header
+
+    $(document).scroll(function () {
+        var stickyHeader = $(document).scrollTop(),
+            header = $(".fr-toolbar");
+
+        if (stickyHeader >= 100) {
+            header.addClass('toolbar-fixed');
+        } else {
+            header.removeClass('toolbar-fixed');
+        }
+    });
+
+
+
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+     $("#menu-toggle-2").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-2");
+        $('#menu ul').hide();
+    });
 
 
 
 
 
-});
-
-// Sticky header
-
-$(document).scroll(function () {
-    var stickyHeader = $(document).scrollTop(),
-        header = $(".fr-toolbar");
-
-    if (stickyHeader >= 100) {
-        header.addClass('toolbar-fixed');
-    } else {
-        header.removeClass('toolbar-fixed');
-    }
 });
